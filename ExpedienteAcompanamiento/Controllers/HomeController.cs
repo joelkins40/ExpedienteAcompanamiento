955d46e6a70376c7d6ead441c423d17876c78f6f
DESKTOP-QTEN7RB\Joel <joel.vargas@udem.edu.mx>
2024-05-13 23:47:01 -0600

    Se agrega funcion para la obtención del token

--- ExpedienteAcompanamiento/Controllers/HomeController.cs
+++ ExpedienteAcompanamiento/Controllers/HomeController.cs
@@ -5,15 +5,27 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace ExpedienteAcompanamiento.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {            
+        public async Task<ActionResult> Index()
+        {
+           string matricula= await  AccesoService.obtenerMatriculaToken();
+
+            FormsAuthenticationTicket sesionTicket = new FormsAuthenticationTicket(1, matricula, DateTime.Now, DateTime.Now.AddMinutes(FormsAuthentication.Timeout.TotalMinutes), true, String.Join(";", "sesion.Roles"));
+            string encryptedTicket = FormsAuthentication.Encrypt(sesionTicket);
+            HttpCookie galletaSesion = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+            Response.Cookies.Add(galletaSesion);
+
+            // Guardamos el token de sesión en una galleta
+            Response.Cookies["_token"].Value = "sesion.Token";
+            Response.Cookies["_token"].Expires = galletaSesion.Expires;
             return View();
         }
 
@@ -21,6 +33,7 @@ namespace ExpedienteAcompanamiento.Controllers
         [ActionName("ObtenerInformacionPersonal")]
         public string Get(string id)
         {
+
             ResultObject response = PersonalesService.ObtenerInformacionPersonal(510830);
             return JsonConvert.SerializeObject(response);
         }
---
+++ ExpedienteAcompanamiento/Models/Services/AccesoService.cs
@@ -0,0 +1,92 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ExpedienteAcompanamiento.Models.Services
+{
+    public class AccesoService
+    {
+        private static readonly string _conString = ConfigurationManager.ConnectionStrings["BANNER"].ConnectionString;
+
+        public static async Task<string> obtenerMatriculaToken()
+        {
+            string token = HttpContext.Current.Request["token"];
+            string tokenAbierto = Encoding.UTF8.GetString(Convert.FromBase64String(token));
+            NameValueCollection parsedValues = HttpUtility.ParseQueryString(tokenAbierto);
+            string matricula =  parsedValues["matricula"];
+            string pin       =  parsedValues["pin"];
+
+            bool valida = await ValidaCredenciales(matricula, pin);
+
+            if (valida)
+            {
+                return matricula;
+            }
+            else
+            {
+                return null;
+            }
+           
+        }
+        public async static Task<bool> ValidaCredenciales(string matricula, string pin)
+        {
+            bool validas = false;
+            try
+            {
+
+
+                using (var connection = new OracleConnection(_conString))
+                {
+                    OracleCommand command = new OracleCommand("SZ_BFQ_KARDEXBA.f_validar_credenciales", connection)
+                    {
+                        CommandType = System.Data.CommandType.StoredProcedure,
+                        BindByName = true
+                    };
+
+                    command.Parameters.Add(new OracleParameter("salida", OracleDbType.Varchar2, 1)
+                    {
+                        Direction = System.Data.ParameterDirection.ReturnValue
+                    });
+                    command.Parameters.Add(new OracleParameter("p_matricula", OracleDbType.Varchar2, 200)
+                    {
+                        Value = matricula,
+                        Direction = System.Data.ParameterDirection.Input
+                    });
+
+                    command.Parameters.Add(new OracleParameter("p_pin", OracleDbType.Varchar2, 200)
+                    {
+                        Value = pin,
+                        Direction = System.Data.ParameterDirection.Input
+                    });
+
+                    connection.Open();
+
+                    int ejecucion = await command.ExecuteNonQueryAsync();
+
+                    try
+                    {
+                        if (Convert.ToString(command.Parameters["salida"]?.Value) == "Y")
+                        {
+                            validas = true;
+                        }
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var error = ex;
+            }
+            return validas;
+        }
+    }
+}
