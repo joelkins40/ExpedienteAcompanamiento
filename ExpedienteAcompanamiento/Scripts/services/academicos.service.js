d551bfc3cf080a55e3bc8285b9b3e449dd1d0d7d
Carlos Perez <caanpesa1@gmail.com>
2024-05-28 08:17:46 -0600

    datos academicos

--- ExpedienteAcompanamiento/Scripts/services/academicos.service.js
+++ ExpedienteAcompanamiento/Scripts/services/academicos.service.js
@@ -1,19 +1,27 @@
 const getAcademicInformation = async () => {
-    try {
-        const response = await axios.get("/DatosAcademicos/ObtenerInformacionDatosAcademicos");
-        debugger;
-        const {
-            DocumentosEntregados,
-            becasPeriodos,
-            datosAdministrativos,
-            bloqueos
-        } = response.data.Value;
+  try {
+    const response = await axios.get(
+      "/DatosAcademicos/ObtenerInformacionDatosAcademicos"
+    );
+    const { datosAcademicos, estatusAlumno, toefl } = response.data.Value;
 
-        //const dateAux = datosAdministrativos[0].TERMINOS_FECHA ? moment(datosAdministrativos[0].TERMINOS_FECHA).format("DD/MMMM/YYYY") : "";
-        return {
-        };
-    } catch (err) {
-        console.error(err);
-        return undefined;
-    }
+    return {
+      datosAcademicos: {
+        creditosAprobados: datosAcademicos[0].CREDITOS_APROBADOS,
+        creditosRequeridos: datosAcademicos[0].CREDITOS_REQUERIDOS,
+        materiasBajas: datosAcademicos[0].MATERIAS_BAJA,
+        programa: datosAcademicos[0].PROGRAMA,
+        servicioSocial: datosAcademicos[0].SERV_SOCIAL,
+        toeflInd: datosAcademicos[0].TOEFL_IND,
+        toeflCode: toefl[0].SORTEST_TESC_CODE,
+        toeflTestDate: toefl[0].SORTEST_TEST_DATE,
+        toeflTestScore: toefl[0].SORTEST_TEST_SCORE,
+        toeflTestDesc: toefl[0].STVTESC_DESC,
+      },
+      estatusAlumno: estatusAlumno,
+    };
+  } catch (err) {
+    console.error(err);
+    return undefined;
+  }
 };
