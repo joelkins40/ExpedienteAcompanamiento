2ae88c7dc20f63a1257b6ee9c1fcf2add66bab8e
lisgtz <lisseth.proyectos@gmail.com>
2024-06-24 10:26:24 -0600

    Avisos de ausencia en residencia

---
+++ ExpedienteAcompanamiento/Controllers/ResidenciasController.cs
@@ -0,0 +1,31 @@
+﻿using ExpedienteAcompanamiento.Models.Services;
+using ExpedienteAcompanamiento.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExpedienteAcompanamiento.Controllers
+{
+    public class ResidenciasController : Controller
+    {
+        // GET: Residencias
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [ActionName("ObtenerAlertas")]
+        public string Get()
+        {
+            int pidm = Convert.ToInt32(Session["pidm"]);
+            //string term = Session["term"].ToString();
+            //string term = "";
+            ResultObject response = ResidenciasAlertasService.ObtenerInformacionDatosAdmision(1);
+            return JsonConvert.SerializeObject(response);
+        }
+    }
+}
---
+++ ExpedienteAcompanamiento/Models/Entity/ResidenciasAlertas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExpedienteAcompanamiento.Models.Entity
+{
+    //public class ResidenciasAlertas
+    //{
+    //    public List<Alertas> residencias_alertas { get; set; }
+    //}
+    public class ResidenciasAlertas
+    {
+        public string Id_Aviso { get; set; }
+        public string Matricula { get; set; }
+        public string Fecha_Salida { get; set; }
+        public string Fecha_Entrada { get; set; }
+        public string Motivo { get; set; }
+        public string FechaCreacion { get; set; }
+        public string Nombre { get; set; }
+        public string Estatus { get; set; }
+        public string Comentario { get; set; }
+        public string Proceso { get; set; }
+        public string FechaDeFinalizacion { get; set; }
+    }
+}
